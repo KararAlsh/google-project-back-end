87e5a13ea213919b495e0f32f7d6da7d4ae09484
KararAlsh <kararabdulsahib@hotmail.com>
2022-09-12 16:26:53 +0700

    imported the css files to the public folder, also added the route

--- app.js
+++ app.js
@@ -43,9 +43,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 // Create route for search
 app.get('/', (req, res) => {
-  res.send('This is the Search Page')
+  res.render('search')
 })
 // Create route for results
+app.get('/Results', (req, res) => {
+  res.render('results')
+})
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
